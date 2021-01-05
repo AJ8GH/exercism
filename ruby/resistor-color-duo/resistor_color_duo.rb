1c613f239694c0a891bc031c4adfe871dcb6fb3f
Adam Jonas <adamjonas25@gmail.com>
2021-01-05 10:03:46 +0000

    Refactor resistor_color_dud.rb

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -6,22 +6,3 @@ class ResistorColorDuo
     Integer [BANDS.index(duo.first), BANDS.index(duo.last)].join
   end
 end
-
-# Hash method using iteration
-# class ResistorColorDuo
-#   BANDS = {
-#      black: 0,
-#      brown: 1,
-#      red: 2,
-#      orange: 3,
-#      yellow: 4,
-#      green: 5,
-#      blue: 6,
-#      violet: 7,
-#      grey: 8,
-#      white: 9
-#    }
-#   def self.value(colors)
-#   Integer colors.take(2).map(&:to_sym).map(&BANDS).join
-#   end
-# end
