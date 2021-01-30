46a18524d0061983103b4059016dd999438be382
Adam Jonas <adamjonas25@gmail.com>
2021-01-30 09:10:11 +0000

    refactor triangle and high scores

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,15 +1,15 @@
 class HighScores
-  attr_reader :scores, :latest, :personal_best, :personal_top_three,
+  attr_reader :scores, :latest, :personal_best, :personal_top_three, :latest_is_personal_best
 
   def initialize(scores)
     @scores = scores
     @latest = scores.last
     @personal_best = scores.max
     @personal_top_three = scores.max(3)
-    @latest_is_personal_best = latest_is_personal_best?
+    @latest_is_personal_best ||= latest == personal_best
   end
 
   def latest_is_personal_best?
-    latest == personal_best
+    latest_is_personal_best
   end
 end
