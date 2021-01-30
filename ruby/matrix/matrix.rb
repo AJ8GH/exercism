81195c42b0e5b27e10d5692373638e2955a76a89
Adam Jonas <adamjonas25@gmail.com>
2021-01-30 17:02:44 +0000

    refactor matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,7 +1,7 @@
 class Matrix
   attr_accessor :rows, :columns
-  def initialize(string)
-    @rows = string.split("\n").map { |row| row.split.map(&:to_i) }
+  def initialize(matrix_string)
+    @rows = matrix_string.lines.map { |row| row.split.map(&:to_i) }
     @columns = rows.transpose
   end
 end
