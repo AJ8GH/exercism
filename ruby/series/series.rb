65a91cf9981c8380493d1d70ea1a8c27ab783619
Adam Jonas <adamjonas25@gmail.com>
2021-01-30 22:35:37 +0000

    refactor series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,24 +1,25 @@
 class Series
   private
 
-  attr_reader :numbers, :numbers_length
-  def initialize(numbers)
-    @numbers = numbers
-    @numbers_length = numbers.length
+  attr_reader :number_string, :number_string_length
+  def initialize(number_string)
+    @number_string = number_string
+    @number_string_length = number_string.length
   end
 
-  def slice_greater_than_length(slice_size)
-    raise ArgumentError if slice_size > numbers_length
+  def argument_length_valid(sub_string_length)
+    raise ArgumentError if sub_string_length > number_string_length
   end
 
   public
 
-  def slices(slice_size)
-    slice_greater_than_length(slice_size)
-    slices = []
-    (numbers_length - slice_size + 1).times do |index|
-      slices << numbers[index, slice_size]
+  def slices(sub_string_length)
+    argument_length_valid(sub_string_length)
+    difference = number_string_length - sub_string_length
+    sub_strings = []
+    (difference + 1).times do |start_of_sub_string|
+      sub_strings << number_string[start_of_sub_string, sub_string_length]
     end
-    slices
+    sub_strings
   end
 end
