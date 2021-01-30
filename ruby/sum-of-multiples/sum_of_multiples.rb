b92318860e93305a6ed8191d7a5852deb9949407
Adam Jonas <adamjonas25@gmail.com>
2021-01-30 16:27:29 +0000

    solve sum_of_multiples

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,8 +1,19 @@
 class SumOfMultiples
-  def initialize(*numbers)
-
+  attr_reader :factors
+  def initialize(*factors)
+    @factors = factors
   end
 
   def to(number)
+    multiples = factors.map do |factor|
+      (1...number).select { |divisor| divisor.multiple?(factor) }
+    end
+    multiples.flatten.uniq.sum
+  end
+end
+
+class Integer
+  def multiple?(factor)
+    self % factor == 0
   end
 end
