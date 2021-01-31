d8fa09dc4a732907cd55c3cc2bbfd84684fae297
Adam Jonas <adamjonas25@gmail.com>
2021-01-31 13:02:32 +0000

    solve grains

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,5 +1,16 @@
 class Grains
+  @@board = 1..64
+
   def self.square(number)
-    number
+    validate(number)
+    2 ** (number - 1)
+  end
+
+  def self.total
+    @@board.map { |number| square(number) }.sum
+  end
+
+  def self.validate(number)
+    raise ArgumentError unless @@board.include?(number)
   end
 end
