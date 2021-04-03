d5a0b7d77252b7ede6db3fabc76e143641d07de8
Adam Jonas <adamjonas25@gmail.com>
2021-04-03 16:33:51 +0100

    Implements half of functionality

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,13 @@
 def is_pangram(sentence):
-    pass
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+    characters = ''.join(set(sentence)).lower()
+
+    import re
+    re.sub(r'[^a-zA-Z]', '', characters)
+
+
+    if len(characters) == 26:
+        return True
+
+    return False
