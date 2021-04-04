be04c5f012be407e65b682b3ba3b9f332bbf46d3
Adam Jonas <adamjonas25@gmail.com>
2021-04-04 12:17:40 +0100

    Completes python protein-translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,25 @@
+import re
+
+CODONS = {
+    'Methionine': ['AUG'],
+    'Phenylalanine': ['UUU', 'UUC'],
+    'Leucine': ['UUA', 'UUG'],
+    'Serine': ['UCU', 'UCC', 'UCA', 'UCG'],
+    'Tyrosine': ['UAU', 'UAC'],
+    'Cysteine': ['UGU', 'UGC'],
+    'Tryptophan': ['UGG'],
+    'STOP': ['UAA', 'UAG', 'UGA']
+}
+
 def proteins(strand):
-    pass
+    sequence = re.findall('...', strand)
+    polypeptide = []
+
+    for codon in sequence:
+        if codon in CODONS['STOP']:
+            break
+        for protein in CODONS:
+            if codon in CODONS[protein]:
+                polypeptide.append(protein)
+
+    return polypeptide
