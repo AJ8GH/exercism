3918c38a641f40f2594c7d4452b47aa42962d87e
Adam Jonas <adamjonas25@gmail.com>
2021-04-04 15:29:38 +0100

    Completes python - robot name

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,27 @@
+import random
+import string
+
 class Robot:
+    names = []
+
     def __init__(self):
-        pass
+        self.generate_name()
+
+    def generate_name(self):
+        random.seed(random.random())
+        name = ''
+        while len(name) < 2:
+            name += random.choice(string.ascii_uppercase)
+        while len(name) < 5:
+            name += str(random.randint(0, 9))
+        self.validate_uniqueness(name)
+
+    def validate_uniqueness(self, name):
+        if name not in self.names:
+            self.names.append(name)
+            self.name = name
+        else:
+            self.generate_name()
+
+    def reset(self):
+        self.generate_name()
