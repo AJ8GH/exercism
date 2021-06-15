a6270542b7939397081bb55624dcb391219cad76
Adam Jonas <adamjonas25@gmail.com>
2021-06-15 14:35:14 +0100

    Solve rna

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class RnaTranscription {
-
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        HashMap<String, String> rna = new HashMap<>();
+        rna.put("G", "C");
+        rna.put("C", "G");
+        rna.put("T", "A");
+        rna.put("A", "U");
+        rna.put("", "");
 
+        List<String> strands = Arrays.asList(dnaStrand.split(""));
+        List<String> transcribed = strands.stream()
+                .map(rna::get)
+                .collect(Collectors.toList());
+        System.out.println(transcribed);
+
+        return String.join("", transcribed);
+    }
 }
