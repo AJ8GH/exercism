47e7cd58be98319f33b01e17aa3011fbdc975917
Adam Jonas <adamjonas25@gmail.com>
2021-06-15 15:17:31 +0100

    Solve Pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,14 @@
+import java.util.*;
+
 public class PangramChecker {
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String filteredInput = input.toLowerCase(Locale.ROOT).replaceAll("[^a-zA-Z]", "");
+        List<String> letters = Arrays.asList(filteredInput.split(""));
+        List<String> uniqueLetters = new ArrayList<>(new HashSet<>(letters));
+        Collections.sort(uniqueLetters);
+        String pangramCheck = String.join("", uniqueLetters);
+        return pangramCheck.equals(alphabet);
     }
-
 }
