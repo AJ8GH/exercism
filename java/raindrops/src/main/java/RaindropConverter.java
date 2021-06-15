4eca9c8b323d84e3edcb9226ac082c4727e0d6c3
Adam Jonas <adamjonas25@gmail.com>
2021-06-15 19:47:49 +0100

    Refactor raindrops java

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,23 +1,10 @@
 class RaindropConverter {
     String convert(int number) {
         StringBuilder output = new StringBuilder();
-        if (isDivisibleBy3(number)) { output.append("Pling"); }
-        if (isDivisibleBy5(number)) { output.append("Plang"); }
-        if (isDivisibleBy7(number)) { output.append("Plong"); }
-        if (!isDivisibleBy3(number) && !isDivisibleBy5(number) && !isDivisibleBy7(number)) {
-            output.append(Integer.toString(number));
-        }
+        if (number % 3 == 0) { output.append("Pling"); }
+        if (number % 5 == 0) { output.append("Plang"); }
+        if (number % 7 == 0) { output.append("Plong"); }
+        if (output.length() == 0) { output.append(number); }
         return output.toString();
-    }
-
-    private boolean isDivisibleBy3(int number) {
-        return number % 3 == 0;
-    }
-
-    private boolean isDivisibleBy5(int number) {
-        return number % 5 == 0;
-    }
-    private boolean isDivisibleBy7(int number) {
-        return number % 7 == 0;
     }
 }
