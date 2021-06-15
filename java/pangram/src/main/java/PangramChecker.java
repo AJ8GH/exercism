cec38f6c33817dd1f20509533f27b10403b1a0e2
Adam Jonas <adamjonas25@gmail.com>
2021-06-15 15:30:18 +0100

    Refactor Pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,14 +1,23 @@
 import java.util.*;
 
 public class PangramChecker {
-    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
     public boolean isPangram(String input) {
-        String filteredInput = input.toLowerCase(Locale.ROOT).replaceAll("[^a-zA-Z]", "");
-        List<String> letters = Arrays.asList(filteredInput.split(""));
+        String normalizedInput = normalize(input);
+        String pangramCheck = createPangramCheck(normalizedInput);
+        return pangramCheck.equals(ALPHABET);
+    }
+
+    private String normalize(String input) {
+        String lowerCaseInput = input.toLowerCase(Locale.ROOT);
+        return lowerCaseInput.replaceAll("[^a-zA-Z]", "");
+    }
+
+    private String createPangramCheck(String normalizedInput) {
+        List<String> letters = Arrays.asList(normalizedInput.split(""));
         List<String> uniqueLetters = new ArrayList<>(new HashSet<>(letters));
         Collections.sort(uniqueLetters);
-        String pangramCheck = String.join("", uniqueLetters);
-        return pangramCheck.equals(alphabet);
+        return String.join("", uniqueLetters);
     }
 }
