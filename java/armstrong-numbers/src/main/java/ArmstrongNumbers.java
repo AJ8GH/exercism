e5af2c12a770e59766fdd29e66efcf8cc4a84f1a
Adam Jonas <adamjonas25@gmail.com>
2021-06-15 11:13:59 +0100

    Solve armstrong numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class ArmstrongNumbers {
-
     boolean isArmstrongNumber(int numberToCheck) {
+        String numberString = String.valueOf(numberToCheck);
+        int length = numberString.length();
 
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] chars = numberString.split("");
+        List<String> digits = new ArrayList<>(Arrays.asList(chars));
 
+        Double armstrong = digits.stream()
+                .map((digit) -> Math.pow(Integer.parseInt(digit), length))
+                .reduce((double) 0, Double::sum);
+
+        return armstrong == numberToCheck;
     }
-
 }
