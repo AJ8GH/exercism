5c9f00b78f9a06295fec562a298e268e8180cd78
Adam Jonas <adamjonas25@gmail.com>
2021-06-16 10:25:14 +0100

    Refactor isogram java

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
 class IsogramChecker {
-
     boolean isIsogram(String phrase) {
-        String letters = phrase.replaceAll("[^a-zA-Z]", "");
-        char[] characters = letters.toLowerCase(Locale.ROOT).toCharArray();
+        char[] characters = normalize(phrase).toCharArray();
         Set<Character> set = new HashSet<>();
-
         for (char character : characters) {
             if (!set.add(character)) { return false; }
         }
         return true;
+    }
+
+    private String normalize(String phrase) {
+        return phrase.replaceAll("[^a-zA-Z]", "").toLowerCase(Locale.ROOT);
     }
 }
