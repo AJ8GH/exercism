73ddbafd7bd52ecea25578637da913957aeba41b
Adam Jonas <adamjonas25@gmail.com>
2021-06-16 12:33:27 +0100

    Solve anagram Java

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,10 +1,36 @@
-/*
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Anagram {
+    private final String word;
+    private final String normalizedWord;
 
-Please remove this comment when submitting your solution.
+    Anagram(String word) {
+        this.word = word;
+        this.normalizedWord = normalize(word);
+    }
 
-*/
+    public List<String> match(List<String> args) {
+        List<String> matches = new ArrayList<>();
+
+        for (String wordToCheck: args) {
+            String normalizedWordToCheck = normalize(wordToCheck);
+            if (!isSameWord(wordToCheck) && normalizedWordToCheck.equals(normalizedWord)) {
+                matches.add(wordToCheck);
+            }
+        }
+        return matches;
+    }
+
+    private String normalize(String word) {
+        char[] lowerCaseChars = word.toLowerCase(Locale.ROOT).toCharArray();
+        Arrays.sort(lowerCaseChars);
+        return new String(lowerCaseChars);
+    }
+
+    private boolean isSameWord(String wordToCheck) {
+        return wordToCheck.toLowerCase(Locale.ROOT).equals(word.toLowerCase(Locale.ROOT));
+    }
+}
