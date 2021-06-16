0f5f38a7896f55f4e2ff84498067061ea0166004
Adam Jonas <adamjonas25@gmail.com>
2021-06-16 12:02:29 +0100

    Solve pig latin in java

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,10 +1,44 @@
-/*
+import java.util.*;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class PigLatinTranslator {
+    private final char[] VOWELS = {'a', 'e', 'i', 'o', 'u', 'y'};
 
-Please remove this comment when submitting your solution.
+    public String translate(String phrase) {
+        String[] words = phrase.split(" ");
 
-*/
+        List<String> translation = new ArrayList<>();
+
+        for (String word: words) {
+            int firstVowel = findFirstVowel(word);
+
+            if (firstVowel == 0 || word.startsWith("xr")) {
+                translation.add(word + "ay");
+            } else if (word.startsWith("qu")) {
+                translation.add(word.substring(2) + "quay");
+            } else if (word.startsWith("squ")) {
+                translation.add(word.substring(3) + "squay");
+            } else {
+                translation.add(
+                        word.substring(firstVowel) +
+                        word.substring(0, firstVowel) +
+                        "ay"
+                );
+            }
+        }
+        return String.join(" ", translation);
+    }
+
+    private int findFirstVowel(String word) {
+        List<Integer> vowelIndices = new ArrayList<>();
+        for (char vowel : VOWELS) {
+            if (word.indexOf(vowel) != -1) {
+                vowelIndices.add(word.indexOf(vowel));
+            }
+        }
+        Collections.sort(vowelIndices);
+        if (word.startsWith("y") && vowelIndices.get(1) == 1) {
+            return vowelIndices.get(1);
+        }
+        return vowelIndices.get(0);
+    }
+}
