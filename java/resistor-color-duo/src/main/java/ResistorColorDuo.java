c739c0d85c2310d43f7e399eee2f96b67d4ba91c
Adam Jonas <adamjonas25@gmail.com>
2021-06-17 20:46:03 +0100

    Solve resistor color duo

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class ResistorColorDuo {
+    private final String[] COLORS = new String[] {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+    };
+
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<String> intList = Arrays.stream(colors)
+                .map(color -> String.valueOf(Arrays.asList(COLORS)
+                        .indexOf(color))).collect(Collectors.toList());
+
+        return Integer.parseInt(String.join("", intList)
+                .substring(0, 2));
     }
 }
