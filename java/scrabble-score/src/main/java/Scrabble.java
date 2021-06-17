cbc5adad677f4b900979852c02c080a5eea94933
Adam Jonas <adamjonas25@gmail.com>
2021-06-17 15:03:46 +0100

    Refactor Scrabble

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,8 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 class Scrabble {
     private final String word;
-    private final HashMap<List<Character>, Integer> SCORES = createScoresMap();
+    private final HashMap<Set<Character>, Integer> SCORES = createScoresMap();
 
     Scrabble(String word) {
         this.word = word;
@@ -15,22 +12,22 @@ class Scrabble {
         char[] chars = word.toUpperCase(Locale.ROOT).toCharArray();
         int score = 0;
         for (char c : chars) {
-            for (List<Character> letters : SCORES.keySet()) {
+            for (Set<Character> letters : SCORES.keySet()) {
                 if(letters.contains(c)) score += SCORES.get(letters);
             }
         }
         return score;
     }
 
-    private HashMap<List<Character>, Integer> createScoresMap() {
-        HashMap<List<Character>, Integer> scoresMap = new HashMap<>();
-        scoresMap.put(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'), 1);
-        scoresMap.put(Arrays.asList('D', 'G'), 2);
-        scoresMap.put(Arrays.asList('B', 'C', 'M', 'P'), 3);
-        scoresMap.put(Arrays.asList('F', 'H', 'V', 'W', 'Y'), 4);
-        scoresMap.put(Arrays.asList('K'), 5);
-        scoresMap.put(Arrays.asList('J', 'X'), 8);
-        scoresMap.put(Arrays.asList('Q', 'Z'), 10);
+    private HashMap<Set<Character>, Integer> createScoresMap() {
+        HashMap<Set<Character>, Integer> scoresMap = new HashMap<>();
+        scoresMap.put(new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T')), 1);
+        scoresMap.put(new HashSet<>(Arrays.asList('D', 'G')), 2);
+        scoresMap.put(new HashSet<>(Arrays.asList('B', 'C', 'M', 'P')), 3);
+        scoresMap.put(new HashSet<>(Arrays.asList('F', 'H', 'V', 'W', 'Y')), 4);
+        scoresMap.put(new HashSet<>(Arrays.asList('K')), 5);
+        scoresMap.put(new HashSet<>(Arrays.asList('J', 'X')), 8);
+        scoresMap.put(new HashSet<>(Arrays.asList('Q', 'Z')), 10);
         return scoresMap;
     }
 }
