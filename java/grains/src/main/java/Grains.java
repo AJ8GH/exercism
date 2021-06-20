4ac622fff472516b8b385167630d919cfea4cc37
Adam Jonas <adamjonas25@gmail.com>
2021-06-20 12:38:22 +0100

    Solve grains

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -1,13 +1,23 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 class Grains {
+    private final BigDecimal initialValue = new BigDecimal(2);
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        validateSquareInput(square);
+        return new BigInteger(String.valueOf(initialValue.pow(square -1)));
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        BigInteger output = new BigInteger("0");
+        for (int i = 1; i <= 64; i++) {
+            output = output.add(grainsOnSquare(i));
+        };
+        return output;
     }
 
+    private void validateSquareInput(int square) {
+        if (square < 1 || square > 64) throw new IllegalArgumentException("square must be between 1 and 64");
+    }
 }
