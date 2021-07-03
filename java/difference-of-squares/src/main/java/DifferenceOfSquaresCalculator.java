27a38a62f0c0d0ca454ff5bd1542426b9e4f0c18
Adam Jonas <adamjonas25@gmail.com>
2021-07-03 22:57:07 +0100

    Refactor Difference of squares

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,23 +1,15 @@
 class DifferenceOfSquaresCalculator {
-    private int sumOfSquares = 0;
-    private int squareOfSum = 0;
-    private boolean squareOfSumComputed;
-    private boolean sumOfSquaresComputed;
 
     int computeSquareOfSumTo(int input) {
-        if (!squareOfSumComputed) {
-            for (int i = 1; i <= input; i++) squareOfSum += i;
-            squareOfSum *= squareOfSum;
-            squareOfSumComputed = true;
-        }
+        int squareOfSum = 0;
+        for (int i = 1; i <= input; i++) squareOfSum += i;
+        squareOfSum *= squareOfSum;
         return squareOfSum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        if(!sumOfSquaresComputed) {
-            for (int i = 1; i <= input; i++) sumOfSquares += i * i;
-            sumOfSquaresComputed = true;
-        }
+        int sumOfSquares = 0;
+        for (int i = 1; i <= input; i++) sumOfSquares += i * i;
         return sumOfSquares;
     }
 
