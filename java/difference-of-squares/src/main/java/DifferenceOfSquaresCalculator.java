4d7ac83165fc4c02d40a943219387a9f7d302238
Adam Jonas <adamjonas25@gmail.com>
2021-07-03 23:08:13 +0100

    create private method to get range

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -3,15 +3,19 @@ import java.util.stream.IntStream;
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        IntStream range = IntStream.rangeClosed(1, input);
+        IntStream range = getRange(input);
         int sum = range.reduce(0, Integer::sum);
         return sum * sum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        IntStream range = IntStream.rangeClosed(1, input);
+        IntStream range = getRange(input);
         return range
                 .reduce(0, (sum, number) -> sum + number * number);
+    }
+
+    private IntStream getRange(int input) {
+        return IntStream.rangeClosed(1, input);
     }
 
     int computeDifferenceOfSquares(int input) {
