be03a686221d2923cd6b36402e0537fd3d88544c
Adam Jonas <adam.jonas@ppb.com>
2021-07-06 22:34:53 +0100

    Solve handshake exercise

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        List<Signal> signals = Arrays.asList(Signal.values());
+        List<Signal> output = new ArrayList<>();
 
+        String binary = Integer.toBinaryString(number);
+
+        int count = 0;
+        for (int i = binary.length() -1; i >= 0; i--) {
+            if (binary.charAt(i) == '1') {
+                if (count == 4) {
+                    Collections.reverse(output);
+                } else {
+                    output.add(signals.get(count));
+                }
+            }
+            count++;
+        }
+        return output;
+    }
 }
