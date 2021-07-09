bd25f9e33168af1dab9a74a166bf8fbea37ad31b
Adam Jonas <adamjonas25@gmail.com>
2021-07-09 23:05:33 +0100

    Solve microblog

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -1,5 +1,12 @@
 class MicroBlog {
+
     public String truncate(String input) {
-        return input;
+        if (Character.codePointCount(input, 0, input.length()) <= 5) {
+            return input;
+        }
+
+        StringBuilder result = new StringBuilder(5);
+        input.codePoints().limit(5).forEach(result::appendCodePoint);
+        return result.toString();
     }
 }
