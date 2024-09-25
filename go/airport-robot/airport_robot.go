909d25c8384853d7392b1122e2848a5d2fbb160a
Adam Jonas <adamjonas25@gmail.com>
2024-09-25 10:36:21 +0100

    Complete airport-robot

--- go/airport-robot/airport_robot.go
+++ go/airport-robot/airport_robot.go
@@ -1,5 +1,32 @@
 package airportrobot
 
-// Write your code here.
-// This exercise does not have tests for each individual task.
-// Try to solve all the tasks first before running the tests.
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, g Greeter) string {
+	return g.Greet(name)
+}
+
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "I can speak Italian: "
+}
+
+func (i Italian) Greet(name string) string {
+	return i.LanguageName() + fmt.Sprintf("Ciao %v!", name)
+}
+
+type Portuguese struct{}
+
+func (p Portuguese) LanguageName() string {
+	return "I can speak Portuguese: "
+}
+
+func (p Portuguese) Greet(name string) string {
+	return p.LanguageName() + fmt.Sprintf("Olá %v!", name)
+}
