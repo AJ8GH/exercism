958297e89c7927b5e384b2ca97d83604518f01b2
Adam Jonas <adamjonas25@gmail.com>
2024-09-27 21:08:55 +0100

    Complete hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,22 @@
 package hamming
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if utf8.RuneCountInString(a) != utf8.RuneCountInString(b) {
+		return 0, errors.New("lengths don't match")
+	}
+
+	count := 0
+	runesA := []rune(a)
+	runesB := []rune(b)
+	for i := 0; i < utf8.RuneCountInString(a); i++ {
+		if runesA[i] != runesB[i] {
+			count++
+		}
+	}
+	return count, nil
 }
