abff33438d1a706ef0102a058e21c96fb0f0f178
Adam Jonas <adamjonas25@gmail.com>
2024-09-27 13:23:52 +0100

    Complete collatz-conjecture

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,21 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("n must not be zero")
+	}
+	for i := 0; true; i++ {
+		if n == 1 {
+			return i, nil
+		}
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			n *= 3
+			n++
+		}
+	}
+	return 0, errors.New("never reached one")
 }
