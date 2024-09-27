fa91172cc7796617e3d330136e8ae14ba5768b78
Adam Jonas <adamjonas25@gmail.com>
2024-09-27 21:50:15 +0100

    Complete isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,18 @@
 package isogram
 
+import (
+	"regexp"
+	"strings"
+)
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	m := map[rune]bool{}
+	re := regexp.MustCompile(`[-\s\t\n\r]+`)
+	for _, v := range strings.ToLower(word) {
+		if m[v] && !re.MatchString(string(v)) {
+			return false
+		}
+		m[v] = true
+	}
+	return true
 }
