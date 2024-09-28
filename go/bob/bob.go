21fab9ec6c59bd28804001e420eccfc4dbfcc344
Adam Jonas <adamjonas25@gmail.com>
2024-09-28 21:39:31 +0100

    Complete bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -5,11 +5,36 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
+import (
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+var ws = regexp.MustCompile(`^[\s\t\n\r]*$`)
+var hl = regexp.MustCompile(`[a-zA-Z]+`)
+
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	switch {
+	case ws.MatchString(remark):
+		return "Fine. Be that way!"
+	case isQuestion(remark) && isYell(remark):
+		return "Calm down, I know what I'm doing!"
+	case isQuestion(remark):
+		return "Sure."
+	case isYell(remark):
+		return "Whoa, chill out!"
+	default:
+		return "Whatever."
+	}
+}
+
+func isQuestion(s string) bool {
+	s = strings.TrimSpace(s)
+	return s[utf8.RuneCountInString(s)-1] == '?'
+}
+
+func isYell(s string) bool {
+	return hl.MatchString(s) && s == strings.ToUpper(s)
 }
