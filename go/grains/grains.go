d46fbb2730b0df86d16037da40e592e935c601f1
Adam Jonas <adamjonas25@gmail.com>
2024-09-28 21:53:20 +0100

    Complete grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,23 @@
 package grains
 
+import "errors"
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number < 1 || number > 64 {
+		return 0, errors.New("make it > 0 and < 64 next time")
+	}
+	out := uint64(1)
+	for i := 0; i < number-1; i++ {
+		out *= 2
+	}
+	return out, nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	sum := uint64(0)
+	for i := 1; i <= 64; i++ {
+		v, _ := Square(i)
+		sum += v
+	}
+	return sum
 }
