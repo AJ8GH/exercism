d82c530e1f0a3789c9a3a54a988dc4693dcb1a1b
Adam Jonas <adamjonas25@gmail.com>
2024-09-28 13:26:05 +0100

    Complete resistor-color

--- go/resistor-color/resistor_color.go
+++ go/resistor-color/resistor_color.go
@@ -1,11 +1,29 @@
 package resistorcolor
 
+var colors = []string{
+	"black",
+	"brown",
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"violet",
+	"grey",
+	"white",
+}
+
 // Colors returns the list of all colors.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	return colors
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	for i, v := range colors {
+		if v == color {
+			return i
+		}
+	}
+	return -1
 }
