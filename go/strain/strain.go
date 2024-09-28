f515e396e981e2398ec20e7b48a5d25e6fab4b6d
Adam Jonas <adamjonas25@gmail.com>
2024-09-28 18:44:03 +0100

    Complete strain

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,6 +1,22 @@
 package strain
 
-// Implement the "Keep" and "Discard" function in this file.
+func Keep[T any](in []T, predicate func(t T) bool) []T {
+	return filter(in, predicate, true)
+}
+
+func Discard[T any](in []T, predicate func(t T) bool) []T {
+	return filter(in, predicate, false)
+}
+
+func filter[T any](in []T, predicate func(t T) bool, predicateResult bool) []T {
+	out := []T{}
+	for _, v := range in {
+		if predicate(v) == predicateResult {
+			out = append(out, v)
+		}
+	}
+	return out
+}
 
 // You will need typed parameters (aka "Generics") to solve this exercise.
 // They are not part of the Exercism syllabus yet but you can learn about
