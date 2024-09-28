8111a3582c1cae4d9d7c973500cdb0c543db4e96
Adam Jonas <adamjonas25@gmail.com>
2024-09-28 20:24:53 +0100

    Complete darts

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,18 @@
 package darts
 
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	switch {
+	case isInCircle(x, y, 1):
+		return 10
+	case isInCircle(x, y, 5):
+		return 5
+	case isInCircle(x, y, 10):
+		return 1
+	default:
+		return 0
+	}
+}
+
+func isInCircle(x, y float64, radius int) bool {
+	return x*x+y*y <= float64(radius*radius)
 }
