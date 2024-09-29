de08406175b0f7d7335f8505e3c3b7e42011e016
Adam Jonas <adamjonas25@gmail.com>
2024-09-29 21:40:23 +0100

    Solve triangle

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -5,24 +5,31 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
-
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind string
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT = "not a triangle"
+	Equ = "equilateral"
+	Iso = "isosceles"
+	Sca = "scalene"
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+	switch {
+	case !isTriangle(a, b, c):
+		return NaT
+	case a == b && b == c:
+		return Equ
+	case a == b || b == c || a == c:
+		return Iso
+	default:
+		return Sca
+	}
+}
+
+func isTriangle(a, b, c float64) bool {
+	return a+b > c && b+c > a && a+c > b
 }
