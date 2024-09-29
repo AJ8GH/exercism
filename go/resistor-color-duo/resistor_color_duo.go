1e2f4cc6897419d5710317877b1f19fbb23caefa
Adam Jonas <adamjonas25@gmail.com>
2024-09-29 13:56:42 +0100

    Solve resistor-color-duo

--- go/resistor-color-duo/resistor_color_duo.go
+++ go/resistor-color-duo/resistor_color_duo.go
@@ -1,6 +1,24 @@
 package resistorcolorduo
 
+import "strconv"
+
+var resistorColorMap = map[string]rune{
+	"black":  '0',
+	"brown":  '1',
+	"red":    '2',
+	"orange": '3',
+	"yellow": '4',
+	"green":  '5',
+	"blue":   '6',
+	"violet": '7',
+	"grey":   '8',
+	"white":  '9',
+}
+
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	panic("Implement the Value function")
+	v, _ := strconv.Atoi(
+		string([]rune{resistorColorMap[colors[0]], resistorColorMap[colors[1]]}),
+	)
+	return v
 }
