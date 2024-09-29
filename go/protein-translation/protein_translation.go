b6f04ea28b91bae0f7c1043300602f5dd52d8999
Adam Jonas <adamjonas25@gmail.com>
2024-09-29 22:51:31 +0100

    Solve protein-translation

--- go/protein-translation/protein_translation.go
+++ go/protein-translation/protein_translation.go
@@ -1,9 +1,48 @@
 package protein
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+var ErrStop = errors.New("stop error")
+var ErrInvalidBase = errors.New("invalid base error")
+
 func FromRNA(rna string) ([]string, error) {
-	panic("Please implement the FromRNA function")
+	out := []string{}
+	for i := 0; i < utf8.RuneCountInString(rna); i += 3 {
+		codon := string(rna[i : i+3])
+		protein, err := FromCodon(codon)
+		if err == ErrStop {
+			return out, nil
+		}
+		if err == ErrInvalidBase {
+			return nil, err
+		}
+		out = append(out, protein)
+	}
+	return out, nil
 }
 
 func FromCodon(codon string) (string, error) {
-	panic("Please implement the FromCodon function")
+	switch codon {
+	case "AUG":
+		return "Methionine", nil
+	case "UUU", "UUC":
+		return "Phenylalanine", nil
+	case "UUA", "UUG":
+		return "Leucine", nil
+	case "UCU", "UCC", "UCA", "UCG":
+		return "Serine", nil
+	case "UAU", "UAC":
+		return "Tyrosine", nil
+	case "UGU", "UGC":
+		return "Cysteine", nil
+	case "UGG":
+		return "Tryptophan", nil
+	case "UAA", "UAG", "UGA":
+		return "", ErrStop
+	default:
+		return "", ErrInvalidBase
+	}
 }
