9cfb547cf52e170c945e7a27490204319f7fd33d
Adam Jonas <adamjonas25@gmail.com>
2024-09-29 11:51:08 +0100

    Solve isbn-verifier

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,32 @@
 package isbn
 
+import (
+	"regexp"
+	"strconv"
+	"unicode/utf8"
+)
+
+var isValidFormat = regexp.MustCompile(`^[\d\-]*X?$`)
+var toRemove = regexp.MustCompile(`[\-]*`)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	if !isValidFormat.MatchString(isbn) {
+		return false
+	}
+
+	isbn = toRemove.ReplaceAllString(isbn, "")
+	if utf8.RuneCountInString(isbn) != 10 {
+		return false
+	}
+
+	sum := 0
+	for i, v := range isbn {
+		if v == 'X' {
+			sum += 10
+		} else {
+			n, _ := strconv.Atoi(string(v))
+			sum += (n * (10 - i))
+		}
+	}
+	return sum%11 == 0
 }
--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -7,6 +7,10 @@ import (
 	"unicode/utf8"
 )
 
+const min = 1
+const max = 3_999
+const base = 10.0
+
 var numeralValues = map[int]rune{
 	1:    'I',
 	5:    'V',
@@ -20,7 +24,7 @@ var numeralValues = map[int]rune{
 var arabicValues = []int{1000, 500, 100, 50, 10, 5, 1}
 
 func ToRomanNumeral(input int) (string, error) {
-	if input < 1 || input > 3_999 {
+	if input < min || input > max {
 		return "", errors.New("make it > 0 and < 4000 next time")
 	}
 
@@ -46,14 +50,14 @@ func handleFourAndNine(numerals []rune, r rune, exponent float64) []rune {
 	n, _ := strconv.Atoi(string(r))
 	return append(
 		numerals,
-		numeralValues[int(math.Pow(10.0, exponent))],
-		numeralValues[(n+1)*int(math.Pow(10.0, exponent))],
+		numeralValues[int(math.Pow(base, exponent))],
+		numeralValues[(n+1)*int(math.Pow(base, exponent))],
 	)
 }
 
 func handleDefault(numerals []rune, r rune, exponent float64) []rune {
 	n, _ := strconv.Atoi(string(r))
-	n *= int(math.Pow(10.0, exponent))
+	n *= int(math.Pow(base, exponent))
 	for _, v := range arabicValues {
 		for divisions := n / v; divisions > 0; divisions-- {
 			numerals = append(numerals, numeralValues[v])
