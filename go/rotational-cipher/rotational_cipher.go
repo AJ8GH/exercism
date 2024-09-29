4c116a04b0043622b9f4ae4b2436a1b6d579ce98
Adam Jonas <adamjonas25@gmail.com>
2024-09-29 12:19:09 +0100

    Solve rotational cipher

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,5 +1,37 @@
 package rotationalcipher
 
+import (
+	"strings"
+	"unicode"
+)
+
+const lettersInAlphabet = 26
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	out := []rune{}
+	for _, r := range plain {
+		rotated := rotate(r, shiftKey)
+		out = append(out, rotated)
+	}
+	return string(out)
+}
+
+func rotate(r rune, shiftKey int) rune {
+	rotate := unicode.ToLower(r)
+	if !strings.ContainsRune(alphabet, rotate) {
+		return r
+	}
+
+	currentIndex := strings.IndexRune(alphabet, rotate)
+	newIndex := currentIndex + shiftKey
+	newIndex %= lettersInAlphabet
+	rotate = rune(alphabet[newIndex])
+
+	if unicode.IsUpper(r) {
+		rotate = unicode.ToUpper(rotate)
+	}
+
+	return rotate
 }
