7723087f30ba9ea2b1203bef1049d4d06c7f1e1c
Adam Jonas <adamjonas25@gmail.com>
2024-10-02 22:45:34 +0100

    Solve atbash-cipher

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,21 @@
 package atbash
 
+import (
+	"regexp"
+	"strings"
+)
+
+var plain = "abcdefghijklmnopqrstuvwxyz0123456789"
+var cipher = "zyxwvutsrqponmlkjihgfedcba0123456789"
+var re = regexp.MustCompile(`\w`)
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	out := []string{}
+	for i, v := range re.FindAllString(strings.ToLower(s), -1) {
+		if i%5 == 0 && i > 0 {
+			out = append(out, " ")
+		}
+		out = append(out, string(cipher[strings.Index(plain, v)]))
+	}
+	return strings.Join(out, "")
 }
