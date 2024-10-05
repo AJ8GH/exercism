8ee9f255791e712a6ff02d4de37f07277fa0b157
Adam Jonas <adamjonas25@gmail.com>
2024-10-05 23:54:51 +0100

    Solve simple-cipher

--- go/simple-cipher/simple_cipher.go
+++ go/simple-cipher/simple_cipher.go
@@ -1,32 +1,115 @@
 package cipher
 
-// Define the shift and vigenere types here.
-// Both types should satisfy the Cipher interface.
+import (
+	"math/rand"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+const lowerA = 97
+const lowerZ = 122
+const alphabetSize = 26
+
+var reInput = regexp.MustCompile(`[a-z]+`)
+var reKeyValid = regexp.MustCompile(`^[a-z]+$`)
+var reKeyInvalid = regexp.MustCompile(`^[a]+$`)
+
+type shift struct {
+	distance int
+}
+
+type vigenere struct {
+	key string
+}
 
 func NewCaesar() Cipher {
-	panic("Please implement the NewCaesar function")
+	return NewShift(3)
 }
 
 func NewShift(distance int) Cipher {
-	panic("Please implement the NewShift function")
+	if distance == 0 || distance >= alphabetSize || distance <= -alphabetSize {
+		return nil
+	}
+	return shift{distance}
 }
 
 func (c shift) Encode(input string) string {
-	panic("Please implement the Encode function")
+	return code(input, c.distance, add)
 }
 
 func (c shift) Decode(input string) string {
-	panic("Please implement the Decode function")
+	return code(input, c.distance, subtract)
+}
+
+func code(input string, distance int, operator func(a, b int) int) string {
+	encoded := []rune{}
+	for _, v := range strings.ToLower(input) {
+		shifted := shiftRune(v, distance, operator)
+		if shifted != -1 {
+			encoded = append(encoded, shifted)
+		}
+	}
+	return string(encoded)
+}
+
+func shiftRune(r rune, distance int, operator func(a, b int) int) rune {
+	if r >= lowerA && r <= lowerZ {
+		shifted := operator(int(r), distance)
+		if shifted < lowerA {
+			shifted += alphabetSize
+		} else if shifted > lowerZ {
+			shifted -= alphabetSize
+		}
+		return rune(shifted)
+	}
+	return -1
+}
+
+func add(r, distance int) int {
+	return r + distance
+}
+
+func subtract(r, distance int) int {
+	return r - distance
 }
 
 func NewVigenere(key string) Cipher {
-	panic("Please implement the NewVigenere function")
+	if !reKeyValid.MatchString(key) || reKeyInvalid.MatchString(key) {
+		return nil
+	}
+	if len(key) == 0 {
+		key = generateKey()
+	}
+	return vigenere{key}
+}
+
+func generateKey() string {
+	key := []rune{}
+	for i := 0; i < 100; i++ {
+		key = append(key, rune(rand.Intn(alphabetSize)+lowerA))
+	}
+	return string(key)
 }
 
 func (v vigenere) Encode(input string) string {
-	panic("Please implement the Encode function")
+	return codeVigenere(input, v.key, add)
 }
 
 func (v vigenere) Decode(input string) string {
-	panic("Please implement the Decode function")
+	return codeVigenere(input, v.key, subtract)
+}
+
+func codeVigenere(input, key string, operator func(a, b int) int) string {
+	input = strings.Join(reInput.FindAllString(strings.ToLower(input), -1), "")
+	encoded := []rune{}
+	for i, r := range input {
+		keyVal := key[i%utf8.RuneCountInString(key)]
+		distance := int(keyVal - lowerA)
+		shifted := shiftRune(r, distance, operator)
+		if shifted != -1 {
+			encoded = append(encoded, shifted)
+		}
+	}
+	return string(encoded)
 }
