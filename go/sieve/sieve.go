7017034e93e4a9f0ddd85c17bc7ca0234c2c1cef
Adam Jonas <adamjonas25@gmail.com>
2024-10-06 09:14:14 +0100

    Solve sieve

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,5 +1,21 @@
 package sieve
 
+var nonPrimes = map[int]bool{}
+
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	primes := []int{}
+	for i := 2; i <= limit; i++ {
+		n := i
+		if nonPrimes[n] {
+			continue
+		}
+
+		primes = append(primes, n)
+		sum := n
+		for sum < limit {
+			sum += n
+			nonPrimes[sum] = true
+		}
+	}
+	return primes
 }
