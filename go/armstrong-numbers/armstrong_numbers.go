f40fd082596737de291b3fa6448674346112e06b
Adam Jonas <adamjonas25@gmail.com>
2024-10-06 09:22:04 +0100

    Solve armstrong-numbers

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,17 @@
 package armstrong
 
+import (
+	"math"
+	"strconv"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	sum := 0
+	nStr := strconv.Itoa(n)
+	digits := float64(len(nStr))
+	for _, v := range nStr {
+		i, _ := strconv.Atoi(string(v))
+		sum += int(math.Pow(float64(i), digits))
+	}
+	return sum == n
 }
