1c2fb1ece8cc7c1a5a09112699b12cb2e68a827a
Adam Jonas <adamjonas25@gmail.com>
2024-10-06 19:29:17 +0100

    Solve series

--- go/series/series.go
+++ go/series/series.go
@@ -1,9 +1,20 @@
 package series
 
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	out := []string{}
+	for i := 0; i <= len(s)-n; i++ {
+		out = append(out, s[i:i+n])
+	}
+	return out
 }
 
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+	return s[:n]
+}
+
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return "", false
+	}
+	return s[:n], true
 }
