ccde17cc969a938a6e9b6d24ee3898e9d06c07e5
Adam Jonas <adamjonas25@gmail.com>
2024-10-06 21:03:06 +0100

    Solve sublist

--- go/sublist/sublist.go
+++ go/sublist/sublist.go
@@ -1,7 +1,39 @@
 package sublist
 
-// Relation type is defined in relations.go file.
-
 func Sublist(l1, l2 []int) Relation {
-	panic("Please implement the Sublist function")
+	switch {
+	case equalSlices(l1, l2):
+		return RelationEqual
+	case len(l1) < len(l2) && isSublist(l1, l2):
+		return RelationSublist
+	case isSublist(l2, l1):
+		return RelationSuperlist
+	default:
+		return RelationUnequal
+	}
+}
+
+func equalSlices[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func isSublist[T comparable](a, b []T) bool {
+	if len(a) == 0 {
+		return true
+	}
+	for i := 0; i <= len(b)-len(a); i++ {
+		if equalSlices(a, b[i:i+len(a)]) {
+			return true
+		}
+	}
+	return false
 }
