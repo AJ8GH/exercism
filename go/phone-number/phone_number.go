88e508d8ba3cd6919dbd30032f68356af81a0130
Adam Jonas <adamjonas25@gmail.com>
2024-10-06 09:38:40 +0100

    Solve phone-number

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -1,13 +1,64 @@
 package phonenumber
 
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const formatPattern = "(%v) %v-%v"
+
+var re = regexp.MustCompile(
+	`^\+?(\d?)[\s\-\.]*\(?([2-9]{1}\d{2})\)?[\s\-\.]*([2-9]{1}\d{2})[\s\-\.]*(\d{4})`,
+)
+
 func Number(phoneNumber string) (string, error) {
-	panic("Please implement the Number function")
+	matches, err := parseAndValidate(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	return strings.Join(matches[1:], ""), nil
 }
 
 func AreaCode(phoneNumber string) (string, error) {
-	panic("Please implement the AreaCode function")
+	matches, err := parseAndValidate(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	return matches[1], nil
 }
 
 func Format(phoneNumber string) (string, error) {
-	panic("Please implement the Format function")
+	matches, err := parseAndValidate(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf(formatPattern, matches[1], matches[2], matches[3]), nil
+}
+
+func parseAndValidate(phoneNumber string) ([]string, error) {
+	if !re.MatchString(phoneNumber) {
+		return nil, errors.New("invalid phone number")
+	}
+
+	matches := re.FindAllStringSubmatch(phoneNumber, -1)[0][1:]
+	countryCode := matches[0]
+	if countryCode != "" && countryCode != "1" {
+		return nil, errors.New("invalid country code")
+	}
+
+	total := 0
+	for _, v := range matches {
+		total += len(v)
+	}
+
+	if total == 11 && countryCode != "1" {
+		return nil, errors.New("11 digit phone number must start with 1")
+	}
+
+	if total > 11 {
+		return nil, errors.New("phone number has more than 11 digits")
+	}
+	return matches, nil
 }
