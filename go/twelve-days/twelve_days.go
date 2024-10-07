10398aabdf6776bae49828c086a9d2b7d5d8ae39
Adam Jonas <adamjonas25@gmail.com>
2024-10-07 22:06:54 +0100

    Solve twelve-days

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -1,9 +1,57 @@
 package twelve
 
+import (
+	"fmt"
+	"strings"
+)
+
+const firstVerse = "On the %v day of Christmas my true love gave to me: "
+
+var lyrics = []string{
+	"twelve Drummers Drumming, ",
+	"eleven Pipers Piping, ",
+	"ten Lords-a-Leaping, ",
+	"nine Ladies Dancing, ",
+	"eight Maids-a-Milking, ",
+	"seven Swans-a-Swimming, ",
+	"six Geese-a-Laying, ",
+	"five Gold Rings, ",
+	"four Calling Birds, ",
+	"three French Hens, ",
+	"two Turtle Doves, ",
+	"and ",
+	"a Partridge in a Pear Tree.",
+}
+
+var days = map[int]string{
+	1:  "first",
+	2:  "second",
+	3:  "third",
+	4:  "fourth",
+	5:  "fifth",
+	6:  "sixth",
+	7:  "seventh",
+	8:  "eighth",
+	9:  "ninth",
+	10: "tenth",
+	11: "eleventh",
+	12: "twelfth",
+}
+
 func Verse(i int) string {
-	panic("Please implement the Verse function")
+	out := []string{fmt.Sprintf(firstVerse, days[i])}
+	start, end := len(lyrics)-i-1, len(lyrics)
+	if i == 1 {
+		start++
+	}
+	out = append(out, lyrics[start:end]...)
+	return strings.Join(out, "")
 }
 
 func Song() string {
-	panic("Please implement the Song function")
+	out := []string{}
+	for i := 1; i <= 12; i++ {
+		out = append(out, Verse(i))
+	}
+	return strings.Join(out, "\n")
 }
