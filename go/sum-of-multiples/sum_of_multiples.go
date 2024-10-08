62c5d99ae07ed6f52855413048c1765178468269
Adam Jonas <adamjonas25@gmail.com>
2024-10-08 21:08:21 +0100

    Solve sum-of-multiples

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,22 @@
 package summultiples
 
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	mults := map[int]bool{}
+	for _, v := range divisors {
+		if v == 0 {
+			continue
+		}
+
+		n := v
+		for n < limit {
+			mults[n] = true
+			n += v
+		}
+	}
+	sum := 0
+	for k := range mults {
+		sum += k
+	}
+
+	return sum
 }
