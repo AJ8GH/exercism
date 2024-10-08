2be9469aba38d1e646d3c4b9fa8284d0d23a504c
Adam Jonas <adamjonas25@gmail.com>
2024-10-08 19:53:55 +0100

    Solve complex-numbers

--- go/complex-numbers/complex_numbers.go
+++ go/complex-numbers/complex_numbers.go
@@ -1,43 +1,55 @@
 package complexnumbers
 
+import "math"
+
 // Define the Number type here.
+type Number struct {
+	a, b float64
+}
 
 func (n Number) Real() float64 {
-	panic("Please implement the Real method")
+	return n.a
 }
 
 func (n Number) Imaginary() float64 {
-	panic("Please implement the Imaginary method")
+	return n.b
 }
 
 func (n1 Number) Add(n2 Number) Number {
-	panic("Please implement the Add method")
+	return Number{n1.a + n2.a, n1.b + n2.b}
 }
 
 func (n1 Number) Subtract(n2 Number) Number {
-	panic("Please implement the Subtract method")
+	return Number{n1.a - n2.a, n1.b - n2.b}
 }
 
 func (n1 Number) Multiply(n2 Number) Number {
-	panic("Please implement the Multiply method")
+	return Number{n1.a*n2.a - n1.b*n2.b, n1.b*n2.a + n1.a*n2.b}
 }
 
 func (n Number) Times(factor float64) Number {
-	panic("Please implement the Times method")
+	return Number{n.a * factor, n.b * factor}
 }
 
 func (n1 Number) Divide(n2 Number) Number {
-	panic("Please implement the Divide method")
+	return Number{
+		(n1.a*n2.a + n1.b*n2.b) / (math.Pow(n2.a, 2) + math.Pow(n2.b, 2)),
+		(n1.b*n2.a - n1.a*n2.b) / (math.Pow(n2.a, 2) + math.Pow(n2.b, 2)),
+	}
 }
 
 func (n Number) Conjugate() Number {
-	panic("Please implement the Conjugate method")
+	return Number{n.a, -n.b}
 }
 
 func (n Number) Abs() float64 {
-	panic("Please implement the Abs method")
+	return math.Sqrt(math.Pow(n.a, 2) + math.Pow(n.b, 2))
 }
 
 func (n Number) Exp() Number {
-	panic("Please implement the Exp method")
+	factor := math.Exp(n.a)
+	return Number{
+		factor * math.Cos(n.b),
+		factor * math.Sin(n.b),
+	}
 }
