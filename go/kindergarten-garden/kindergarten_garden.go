b8474366efc63d38d72a0b3d205fa180a0e75f64
Adam Jonas <adamjonas25@gmail.com>
2024-10-21 21:47:29 +0100

    Solve kindergarten garden

--- go/kindergarten-garden/kindergarten_garden.go
+++ go/kindergarten-garden/kindergarten_garden.go
@@ -1,6 +1,27 @@
 package kindergarten
 
+import (
+	"errors"
+	"regexp"
+	"slices"
+	"strings"
+)
+
+var plants = map[rune]string{
+	'V': "violets",
+	'G': "grass",
+	'C': "clover",
+	'R': "radishes",
+}
+
 // Define the Garden type here.
+type Garden struct {
+	diagram  string
+	children []string
+}
+
+var errBadGarden = errors.New("bad")
+var re = regexp.MustCompile(`^\n[VGCR]{2,}\n[VGCR]{2,}$`)
 
 // The diagram argument starts each row with a '\n'.  This allows Go's
 // raw string literals to present diagrams in source code nicely as two
@@ -11,9 +32,47 @@ package kindergarten
 //     VVCCGG`
 
 func NewGarden(diagram string, children []string) (*Garden, error) {
-	panic("Please implement the NewGarden function")
+	if !re.MatchString(diagram) || !validChildren(children) || !validDiagram(diagram) {
+		return nil, errBadGarden
+	}
+
+	return &Garden{diagram: diagram, children: children}, nil
+}
+
+func validDiagram(diagram string) bool {
+	rows := strings.Split(diagram, "\n")
+	return len(rows[1]) == len(rows[2]) &&
+		len(rows[1])%2 == 0 &&
+		len(rows[2])%2 == 0 &&
+		re.MatchString(diagram)
+}
+
+func validChildren(children []string) bool {
+	found := map[string]bool{}
+	for _, v := range children {
+		if found[v] {
+			return false
+		}
+		found[v] = true
+	}
+	return true
 }
 
 func (g *Garden) Plants(child string) ([]string, bool) {
-	panic("Please implement the Plants function")
+	rows := strings.Split(g.diagram, "\n")
+	children := make([]string, len(g.children))
+	copy(children, g.children)
+	slices.Sort(children)
+	indexOfChild := slices.Index(children, child)
+	if indexOfChild < 0 {
+		return nil, false
+	}
+	var out []string
+
+	out = append(out, plants[rune(rows[1][indexOfChild*2])])
+	out = append(out, plants[rune(rows[1][indexOfChild*2+1])])
+	out = append(out, plants[rune(rows[2][indexOfChild*2])])
+	out = append(out, plants[rune(rows[2][indexOfChild*2+1])])
+
+	return out, true
 }
