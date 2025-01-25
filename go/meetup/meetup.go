986cfb0b516701f480aca378bb7917aee3bdfe43
Adam Jonas <adamjonas25@gmail.com>
2025-01-25 23:47:48 +0000

    Start solving meetup

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -2,8 +2,46 @@ package meetup
 
 import "time"
 
-// Define the WeekSchedule type here.
+type WeekSchedule string
+
+const (
+	First  WeekSchedule = "first"
+	Second WeekSchedule = "second"
+	Third  WeekSchedule = "third"
+	Fourth WeekSchedule = "fourth"
+	Last   WeekSchedule = "last"
+	Teenth WeekSchedule = "teenth"
+)
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	switch wSched {
+	case First:
+		for i := 1; i <= 7; i++ {
+			if isDay(wDay, month, year, i) {
+				return i
+			}
+		}
+		return 0
+	case Second:
+		return 0
+	case Third:
+		return 0
+	case Fourth:
+		return 0
+	case Last:
+		return 0
+	case Teenth:
+		for i := 13; i <= 19; i++ {
+			if isDay(wDay, month, year, i) {
+				return i
+			}
+		}
+		return 0
+	default:
+		panic("bad WeekSchedule")
+	}
+}
+
+func isDay(wd time.Weekday, m time.Month, y int, d int) bool {
+	return false
 }
