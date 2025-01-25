a3d2077eb6c4d127b1e5a115d2dfb5e49d5c78d3
Adam Jonas <adamjonas25@gmail.com>
2025-01-25 22:55:10 +0000

    Solve crypo-square

--- go/crypto-square/crypto_square.go
+++ go/crypto-square/crypto_square.go
@@ -9,49 +9,30 @@ import (
 var re = regexp.MustCompile(`[a-zA-Z0-9]+`)
 
 func Encode(pt string) string {
-	/*
-
-		l -> length of message
-		c -> number of columns
-		r -> number of rows,
-
-		for rectangle  r  x  c  find the smallest possible integer  c  such that:
-
-		r * c >= l
-		c >= r
-		c - r <= 1
-
-		---
-
-		"1, 2, 3 GO!"
-
-		123go
-
-		123
-		go
-
-		1g 2o 3
-
-		---
-
-
-		"12345678"
-
-		123
-		456
-		78
-
-		"147 258 36 "
-
-	*/
-
 	letters := re.FindAllString(pt, -1)
 	normalised := strings.ToLower(strings.Join(letters, ""))
 	l := len(normalised)
 	sq := int(math.Ceil(math.Sqrt(float64(l))))
+	rows := rows(normalised, l, sq)
 
-	var rows []string
+	var out []string
+	for i := 0; i < sq; i++ {
+		var chunk string
+		for _, row := range rows {
+			switch {
+			case i < len(row):
+				chunk += string(row[i])
+			default:
+				chunk += " "
+			}
+		}
+		out = append(out, chunk)
+	}
 
+	return strings.Join(out, " ")
+}
+
+func rows(normalised string, l, sq int) (rows []string) {
 	for i := 0; i < l; i += sq {
 		end := i + sq
 		if end > l {
@@ -63,19 +44,5 @@ func Encode(pt string) string {
 		}
 		rows = append(rows, chunk)
 	}
-
-	out := ""
-	for i := 0; i < sq; i++ {
-		for _, row := range rows {
-			if i < len(row) {
-				out += string(row[i])
-			}
-		}
-		if i == sq-1 {
-			break
-		}
-		out += " "
-	}
-
-	return out
+	return rows
 }
