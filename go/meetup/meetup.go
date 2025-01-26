b5e0a68f94aa576c58e8a6a1c4d73e0af076b047
Adam Jonas <adamjonas25@gmail.com>
2025-01-26 10:30:44 +0000

    Solve meetup

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -16,32 +16,67 @@ const (
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
 	switch wSched {
 	case First:
-		for i := 1; i <= 7; i++ {
-			if isDay(wDay, month, year, i) {
-				return i
-			}
-		}
-		return 0
+		return findDay(year, month, wDay, 1, 7, 1)
 	case Second:
-		return 0
+		return findDay(year, month, wDay, 1, 14, 2)
 	case Third:
-		return 0
+		return findDay(year, month, wDay, 1, 21, 3)
 	case Fourth:
-		return 0
+		return findDay(year, month, wDay, 1, 28, 4)
 	case Last:
-		return 0
+		return findLastDay(year, month, wDay)
 	case Teenth:
-		for i := 13; i <= 19; i++ {
-			if isDay(wDay, month, year, i) {
-				return i
-			}
-		}
-		return 0
+		return findDay(year, month, wDay, 13, 19, 1)
 	default:
 		panic("bad WeekSchedule")
 	}
 }
 
+func findDay(
+	y int,
+	m time.Month,
+	wd time.Weekday,
+	startDay,
+	endDay,
+	occurrence int,
+) int {
+	found := 0
+	for i := startDay; i <= endDay; i++ {
+		if isDay(wd, m, y, i) {
+			found++
+			if found == occurrence {
+				return i
+			}
+		}
+	}
+	return 0
+}
+
+func findLastDay(y int, m time.Month, wd time.Weekday) int {
+	startDay := 31
+	switch m {
+	case time.February:
+		startDay = daysInFeb(y)
+	case time.April, time.June, time.September, time.November:
+		startDay = 30
+	}
+	for i := startDay; i >= startDay-7; i-- {
+		if isDay(wd, m, y, i) {
+			return i
+		}
+	}
+	return 0
+}
+
 func isDay(wd time.Weekday, m time.Month, y int, d int) bool {
-	return false
+	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC).Weekday() == wd
+}
+
+func daysInFeb(y int) int {
+	switch {
+	case y%4 == 0 && (y%100 != 0 || y%400 == 0):
+		return 29
+	default:
+		return 28
+	}
 }
