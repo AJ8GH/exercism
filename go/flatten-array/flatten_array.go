860efb0815f81918acd6ab190da7fb731b37e94a
Adam Jonas <adamjonas25@gmail.com>
2025-02-03 21:10:39 +0000

    Solve flatten-array

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,56 +1,30 @@
 package flatten
 
 import (
-	"fmt"
 	"reflect"
 )
 
-func Flatten(nested interface{}) (out []interface{}) {
+func Flatten(nested interface{}) []interface{} {
+	out := make([]interface{}, 0)
 	val := reflect.ValueOf(nested)
 
 	switch {
 	case val.IsNil():
-		return []interface{}{}
+		return out
 	default:
-		return getAllVals(val, out)
+		return getAll(val, out)
 	}
 }
 
-func getAllVals(val reflect.Value, out []interface{}) []interface{} {
-	if val.IsNil() {
-		return out
-	}
-
-	switch val.Kind() {
-	case reflect.Pointer:
-		val = val.Elem()
-		vk := val.Kind()
-		fmt.Printf("%v", vk)
-		fallthrough
-	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			f := val.Field(i)
-			if f.IsNil() {
-				continue
-			}
-			out = getAllVals(f, out)
+func getAll(val reflect.Value, out []interface{}) []interface{} {
+	for i := 0; i < val.Len(); i++ {
+		v := reflect.ValueOf(val.Index(i).Interface())
+		switch v.Kind() {
+		case reflect.Array, reflect.Slice:
+			out = getAll(v, out)
+		case reflect.Int:
+			out = append(out, int(v.Int()))
 		}
-	case reflect.Slice, reflect.Array:
-		for i := 0; i < val.Len(); i++ {
-			el := val.Index(i)
-			k := el.Kind()
-			fmt.Printf("%v", k)
-			out = getAllVals(el, out)
-		}
-	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
-		out = append(out, val.Int())
-	case reflect.Uint16, reflect.Uint32, reflect.Uint8, reflect.Uint:
-		out = append(out, val.Uint())
-	default:
-		v := val.Interface()
-		vo := reflect.ValueOf(val)
-		println(fmt.Sprintf("%v %v", v, vo))
-		out = append(out, val.Interface())
 	}
 	return out
 }
