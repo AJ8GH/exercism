35fe478d03e95a7b1dab0941da2fc70fe6fca931
Adam Jonas <adamjonas25@gmail.com>
2025-02-04 12:03:20 +0000

    Start solving robot-name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,11 +1,56 @@
 package robotname
 
+import (
+	"fmt"
+	"math/rand"
+)
+
+const (
+	ceil        = 10
+	lenAlpha    = 26
+	alphaOffset = int('A')
+	template    = "%s%s%d%d%d"
+)
+
 // Define the Robot type here.
+type Robot struct {
+	name string
+}
+
+var names = map[string]bool{}
 
 func (r *Robot) Name() (string, error) {
-	panic("Please implement the Name function")
+	if r.name == "" {
+		r.name = genName()
+	}
+	return r.name, nil
 }
 
 func (r *Robot) Reset() {
-	panic("Please implement the Reset function")
+	r.name = genName()
+}
+
+func randLetter() string {
+	n := rand.Intn(lenAlpha) + alphaOffset
+	return string(rune(n))
+}
+
+func genName() string {
+	name := createName()
+	for names[name] {
+		name = createName()
+	}
+	names[name] = true
+	return name
+}
+
+func createName() string {
+	return fmt.Sprintf(
+		template,
+		randLetter(),
+		randLetter(),
+		rand.Intn(ceil),
+		rand.Intn(ceil),
+		rand.Intn(ceil),
+	)
 }
