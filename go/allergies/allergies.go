aab1725c756d5296a13a343917fa77e368271d9e
Adam Jonas <adamjonas25@gmail.com>
2025-02-06 22:39:55 +0000

    Solve allergies

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,9 +1,44 @@
 package allergies
 
-func Allergies(allergies uint) []string {
-	panic("Please implement the Allergies function")
+var allergenMap = map[uint]string{
+	1:   "eggs",
+	2:   "peanuts",
+	4:   "shellfish",
+	8:   "strawberries",
+	16:  "tomatoes",
+	32:  "chocolate",
+	64:  "pollen",
+	128: "cats",
+}
+
+var nums = []uint{128, 64, 32, 16, 8, 4, 2, 1}
+
+func Allergies(allergies uint) (allergens []string) {
+	if allergies > 256 {
+		ref := 128
+		for ref <= int(allergies) {
+			ref *= 2
+		}
+
+		ref /= 2
+		allergies %= uint(ref)
+	}
+
+	for _, v := range nums {
+		if v <= allergies {
+			allergies -= v
+			allergens = append(allergens, allergenMap[v])
+		}
+	}
+
+	return allergens
 }
 
 func AllergicTo(allergies uint, allergen string) bool {
-	panic("Please implement the AllergicTo function")
+	for _, v := range Allergies(allergies) {
+		if v == allergen {
+			return true
+		}
+	}
+	return false
 }
