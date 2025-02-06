cf06c6fbcf51d9b7dd3dff9b533c4bcb232b7820
Adam Jonas <adamjonas25@gmail.com>
2025-02-06 23:05:30 +0000

    Solve perfect-numbers

--- go/perfect-numbers/perfect_numbers.go
+++ go/perfect-numbers/perfect_numbers.go
@@ -1,7 +1,42 @@
 package perfect
 
-// Define the Classification type here.
+import "errors"
+
+type (
+	Classification string
+)
+
+var ErrOnlyPositive = errors.New("n must be positive")
+
+const (
+	ClassificationDeficient = "Defi"
+	ClassificationPerfect   = "Perf"
+	ClassificationAbundant  = "Abun"
+)
 
 func Classify(n int64) (Classification, error) {
-	panic("Please implement the Classify function")
+	if n <= 0 {
+		return "", ErrOnlyPositive
+	}
+	factors := []int64{1}
+	for i := int64(2); i <= n/int64(2); i++ {
+		if n%i == 0 {
+			factors = append(factors, i)
+		}
+	}
+
+	sum := int64(0)
+
+	for _, v := range factors {
+		sum += v
+	}
+
+	switch {
+	case n == 1, sum < n:
+		return ClassificationDeficient, nil
+	case sum == n:
+		return ClassificationPerfect, nil
+	default:
+		return ClassificationAbundant, nil
+	}
 }
