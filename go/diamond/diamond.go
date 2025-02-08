de69110a7b931a5882eef34f3a6f9366bb9537dc
Adam Jonas <adamjonas25@gmail.com>
2025-02-08 17:05:58 +0000

    Refactor

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -7,45 +7,6 @@ const (
 	last  = 'Z'
 )
 
-/*
-
- A -> 0
-
-  A
- B B -> 1
-  A
-
-   A
-  B B
- C   C -> 3
-  B B
-	 A
-
-    A
-   B B
-  C   C
- D     D -> 5
-  C   C
-   B B
-	  A
-
-     A
-    B B
-   C   C
-  D     D
- E       E -> 7
-  D     D
-   C   C
-    B B
-	   A
-
-	0 1 2 3 4 5 6
-  0 1 3 5 7 9 11
-
-	1 2 3 4 5 6
-	0 1 3 5 7 9
-*/
-
 func Gen(char byte) (string, error) {
 	if char < first || char > last {
 		return "", errors.New("bad char")
@@ -57,76 +18,62 @@ func Gen(char byte) (string, error) {
 	diff := int(char) - int(first)
 	padding := diff*2 - 1
 
-	out := ""
+	var out string
 	for i := first; i <= rune(char); i++ {
-		line := ""
-		switch i {
-		case first:
-			for j := 0; j < diff; j++ {
-				line += " "
-			}
-			line += string(rune(i))
-			for j := 0; j < diff; j++ {
-				line += " "
-			}
-		case rune(char):
-			line += string(char)
-			for j := 0; j < padding; j++ {
-				line += " "
-			}
-			line += string(char)
-		default:
-			currentDiff := i - first
-			currentPadding := currentDiff*2 - 1
-			middle := string(i)
-			for j := 0; j < int(currentPadding); j++ {
-				middle += " "
-			}
-			middle += string(i)
-			sidePadding := padding - int(currentPadding)
-			for j := 0; j < sidePadding/2; j++ {
-				line += " "
-			}
-			line += middle
-			for j := 0; j < sidePadding/2; j++ {
-				line += " "
-			}
-		}
-		out += line + "\n"
+		out += buildLine(char, i, diff, padding)
 	}
 
 	for i := rune(char - 1); i >= first; i-- {
-		line := ""
-		switch i {
-		case first:
-			for j := 0; j < diff; j++ {
-				line += " "
-			}
-			line += string(rune(i))
-			for j := 0; j < diff; j++ {
-				line += " "
-			}
-		default:
-			currentDiff := i - first
-			currentPadding := currentDiff*2 - 1
-			middle := string(i)
-			for j := 0; j < int(currentPadding); j++ {
-				middle += " "
-			}
-			middle += string(i)
-			sidePadding := padding - int(currentPadding)
-			for j := 0; j < sidePadding/2; j++ {
-				line += " "
-			}
-			line += middle
-			for j := 0; j < sidePadding/2; j++ {
-				line += " "
-			}
-		}
-		out += line
-		if i != first {
-			out += "\n"
-		}
+		out += buildLine(char, i, diff, padding)
 	}
-	return out, nil
+	return out[:len(out)-1], nil
+}
+
+func buildLine(char byte, i rune, diff, padding int) string {
+	var line string
+	switch i {
+	case first:
+		line = buildFirstLine(diff)
+	case rune(char):
+		line += string(char)
+		for j := 0; j < padding; j++ {
+			line += " "
+		}
+		line += string(char)
+	default:
+		line = buildRegularLine(i, padding)
+	}
+	return line + "\n"
+}
+
+func buildRegularLine(currentChar rune, padding int) string {
+	line := ""
+	currentDiff := currentChar - first
+	currentPadding := currentDiff*2 - 1
+	middle := string(currentChar)
+	for j := 0; j < int(currentPadding); j++ {
+		middle += " "
+	}
+	middle += string(currentChar)
+	sidePadding := padding - int(currentPadding)
+	for j := 0; j < sidePadding/2; j++ {
+		line += " "
+	}
+	line += middle
+	for j := 0; j < sidePadding/2; j++ {
+		line += " "
+	}
+	return line
+}
+
+func buildFirstLine(diff int) string {
+	line := ""
+	for j := 0; j < diff; j++ {
+		line += " "
+	}
+	line += string(first)
+	for j := 0; j < diff; j++ {
+		line += " "
+	}
+	return line
 }
