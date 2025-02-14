3598eb02f37635abbd44c2230af370da9ab56e73
Adam Jonas <adamjonas25@gmail.com>
2025-02-14 21:25:03 +0000

    Solve error-handling

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,5 +1,36 @@
 package erratum
 
-func Use(opener ResourceOpener, input string) error {
-	panic("Please implement the Use function")
+func Use(opener ResourceOpener, input string) (err error) {
+	r, err := openResource(opener)
+	if err != nil {
+		return err
+	}
+
+	defer r.Close()
+	defer func() {
+		if e := recover(); e != nil {
+			f, isFrob := e.(FrobError)
+			if isFrob {
+				r.Defrob(f.defrobTag)
+			}
+			err = e.(error)
+		}
+	}()
+
+	r.Frob(input)
+	return err
+}
+
+func openResource(opener ResourceOpener) (Resource, error) {
+	r, err := opener()
+
+	for err != nil {
+		_, isTransient := err.(TransientError)
+		if !isTransient {
+			return nil, err
+		}
+		r, err = opener()
+	}
+
+	return r, nil
 }
