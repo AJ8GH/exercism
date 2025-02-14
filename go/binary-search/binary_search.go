d3398dbda9de304ce713028a06a1a5d8ac8608a5
Adam Jonas <adamjonas25@gmail.com>
2025-02-14 22:39:42 +0000

    Solve binary-search

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,5 +1,24 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+	upper := len(list) - 1
+	lower := 0
+	i := len(list) / 2
+	for upper >= lower {
+		switch {
+		case upper-lower < 0:
+			return -1
+		case upper-lower == 0 && list[i] != key:
+			return -1
+		case list[i] == key:
+			return i
+		case list[i] > key:
+			upper = i - 1
+			i = upper - (upper-lower)/2
+		case list[i] < key:
+			lower = i + 1
+			i = lower + (upper-lower)/2
+		}
+	}
+	return -1
 }
