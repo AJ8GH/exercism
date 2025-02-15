cc7609880377895eb58a6dd430bc675236d77938
Adam Jonas <adamjonas25@gmail.com>
2025-02-15 17:27:46 +0000

    Solve circular-buffer

--- go/circular-buffer/circular_buffer.go
+++ go/circular-buffer/circular_buffer.go
@@ -1,5 +1,7 @@
 package circular
 
+import "errors"
+
 // Implement a circular buffer of bytes supporting both overflow-checked writes
 // and unconditional, possibly overwriting, writes.
 //
@@ -7,24 +9,69 @@ package circular
 // and io.ByteWriter and can be used (size permitting) as a drop in
 // replacement for anything using that interface.
 
-// Define the Buffer type here.
+type Buffer struct {
+	data     []byte
+	capacity int
+	size     int
+	next     int
+	first    int
+}
 
 func NewBuffer(size int) *Buffer {
-	panic("Please implement the NewBuffer function")
+	return &Buffer{
+		data:     make([]byte, size),
+		capacity: size,
+	}
 }
 
 func (b *Buffer) ReadByte() (byte, error) {
-	panic("Please implement the ReadByte function")
+	if b.size == 0 {
+		return 0, errors.New("no stuff")
+	}
+	val := b.data[b.first]
+	b.data[b.first] = 0
+	b.size--
+	b.first++
+	if b.first >= b.capacity {
+		b.first = 0
+	}
+	return val, nil
 }
 
 func (b *Buffer) WriteByte(c byte) error {
-	panic("Please implement the WriteByte function")
+	if b.size == b.capacity {
+		return errors.New("too much stuff")
+	}
+	b.data[b.next] = c
+	b.size++
+	b.next++
+	if b.next >= b.capacity {
+		b.next = 0
+	}
+	return nil
 }
 
 func (b *Buffer) Overwrite(c byte) {
-	panic("Please implement the Overwrite function")
+	b.data[b.next] = c
+	if b.size < b.capacity {
+		b.size++
+	} else {
+		b.first++
+		if b.first >= b.capacity {
+			b.first = 0
+		}
+	}
+	b.next++
+	if b.next >= b.capacity {
+		b.next = 0
+	}
 }
 
 func (b *Buffer) Reset() {
-	panic("Please implement the Reset function")
+	b.size = 0
+	b.next = 0
+	b.first = 0
+	for i := range b.data {
+		b.data[i] = 0
+	}
 }
