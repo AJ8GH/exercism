d865ddcc2ebf42d426444523b93d1bd83d6c3f52
Adam Jonas <adamjonas25@gmail.com>
2025-03-26 21:01:27 +0000

    Refactor ledger

--- go/ledger/ledger.go
+++ go/ledger/ledger.go
@@ -12,214 +12,225 @@ type Entry struct {
 	Change      int // in cents
 }
 
+type ChanEntry struct {
+	i int
+	s string
+	e error
+}
+
 func FormatLedger(currency string, locale string, entries []Entry) (string, error) {
-	var entriesCopy []Entry
-	for _, e := range entries {
-		entriesCopy = append(entriesCopy, e)
-	}
+	var entriesCopy []Entry = make([]Entry, len(entries))
+	copy(entriesCopy, entries)
+
 	if len(entries) == 0 {
-		if _, err := FormatLedger(currency, "en-US", []Entry{{Date: "2014-01-01", Description: "", Change: 0}}); err != nil {
+		if _, err := FormatLedger(currency, "en-US", []Entry{{
+			Date:        "2014-01-01",
+			Description: "",
+			Change:      0,
+		}}); err != nil {
 			return "", err
 		}
 	}
-	m1 := map[bool]int{true: 0, false: 1}
-	m2 := map[bool]int{true: -1, false: 1}
-	es := entriesCopy
-	for len(es) > 1 {
-		first, rest := es[0], es[1:]
-		success := false
-		for !success {
-			success = true
-			for i, e := range rest {
-				if (m1[e.Date == first.Date]*m2[e.Date < first.Date]*4 +
-					m1[e.Description == first.Description]*m2[e.Description < first.Description]*2 +
-					m1[e.Change == first.Change]*m2[e.Change < first.Change]*1) < 0 {
-					es[0], es[i+1] = es[i+1], es[0]
-					success = false
-				}
-			}
-		}
-		es = es[1:]
+
+	doStuffWithEntries(entriesCopy)
+	out, err := buildOut(locale)
+	if err != nil {
+		return "", err
 	}
 
-	var s string
-	if locale == "nl-NL" {
-		s = "Datum" +
-			strings.Repeat(" ", 10-len("Datum")) +
-			" | " +
-			"Omschrijving" +
-			strings.Repeat(" ", 25-len("Omschrijving")) +
-			" | " + "Verandering" + "\n"
-	} else if locale == "en-US" {
-		s = "Date" +
-			strings.Repeat(" ", 10-len("Date")) +
-			" | " +
-			"Description" +
-			strings.Repeat(" ", 25-len("Description")) +
-			" | " + "Change" + "\n"
-	} else {
-		return "", errors.New("")
-	}
 	// Parallelism, always a great idea
-	co := make(chan struct {
-		i int
-		s string
-		e error
-	})
+	channel := make(chan ChanEntry)
+
 	for i, et := range entriesCopy {
 		go func(i int, entry Entry) {
-			if len(entry.Date) != 10 {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
+			switch {
+			case len(entry.Date) != 10:
+				channel <- ChanEntry{e: errors.New("")}
+			case entry.Date[4] != '-':
+				channel <- ChanEntry{e: errors.New("")}
+			case entry.Date[7] != '-':
+				channel <- ChanEntry{e: errors.New("")}
 			}
-			d1, d2, d3, d4, d5 := entry.Date[0:4], entry.Date[4], entry.Date[5:7], entry.Date[7], entry.Date[8:10]
-			if d2 != '-' {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
-			}
-			if d4 != '-' {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
-			}
-			de := entry.Description
-			if len(de) > 25 {
-				de = de[:22] + "..."
-			} else {
-				de = de + strings.Repeat(" ", 25-len(de))
-			}
-			var d string
-			if locale == "nl-NL" {
-				d = d5 + "-" + d3 + "-" + d1
-			} else if locale == "en-US" {
-				d = d3 + "/" + d5 + "/" + d1
-			}
+
+			date := getDate(locale, entry)
+
 			negative := false
 			cents := entry.Change
 			if cents < 0 {
 				cents = cents * -1
 				negative = true
 			}
-			var a string
-			if locale == "nl-NL" {
-				if currency == "EUR" {
-					a += "€"
-				} else if currency == "USD" {
-					a += "$"
-				} else {
-					co <- struct {
-						i int
-						s string
-						e error
-					}{e: errors.New("")}
-				}
-				a += " "
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + "."
-				}
-				a = a[:len(a)-1]
-				a += ","
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += "-"
-				} else {
-					a += " "
-				}
-			} else if locale == "en-US" {
-				if negative {
-					a += "("
-				}
-				if currency == "EUR" {
-					a += "€"
-				} else if currency == "USD" {
-					a += "$"
-				} else {
-					co <- struct {
-						i int
-						s string
-						e error
-					}{e: errors.New("")}
-				}
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + ","
-				}
-				a = a[:len(a)-1]
-				a += "."
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += ")"
-				} else {
-					a += " "
-				}
+
+			a, al := buildA(locale, currency, channel, cents, negative)
+			desc := entry.Description
+			if len(desc) > 25 {
+				desc = desc[:22] + "..."
 			} else {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
+				desc = desc + strings.Repeat(" ", 25-len(desc))
 			}
-			var al int
-			for range a {
-				al++
+
+			channel <- ChanEntry{
+				i: i,
+				s: date + strings.Repeat(" ", 10-len(date)) + " | " + desc + " | " + strings.Repeat(" ", 13-al) + a + "\n",
 			}
-			co <- struct {
-				i int
-				s string
-				e error
-			}{i: i, s: d + strings.Repeat(" ", 10-len(d)) + " | " + de + " | " +
-				strings.Repeat(" ", 13-al) + a + "\n"}
 		}(i, et)
 	}
+
 	ss := make([]string, len(entriesCopy))
 	for range entriesCopy {
-		v := <-co
+		v := <-channel
 		if v.e != nil {
 			return "", v.e
 		}
+
 		ss[v.i] = v.s
 	}
+
 	for i := 0; i < len(entriesCopy); i++ {
-		s += ss[i]
+		out += ss[i]
 	}
-	return s, nil
+
+	return out, nil
+}
+
+func getCentsStr(cents int) string {
+	centsStr := strconv.Itoa(cents)
+	switch len(centsStr) {
+	case 1:
+		centsStr = "00" + centsStr
+	case 2:
+		centsStr = "0" + centsStr
+	}
+	return centsStr
+}
+
+func buildOut(locale string) (string, error) {
+	var out string
+	switch locale {
+	case "nl-NL":
+		out = "Datum" +
+			strings.Repeat(" ", 10-len("Datum")) +
+			" | " +
+			"Omschrijving" +
+			strings.Repeat(" ", 25-len("Omschrijving")) +
+			" | " + "Verandering" + "\n"
+	case "en-US":
+		out = "Date" +
+			strings.Repeat(" ", 10-len("Date")) +
+			" | " +
+			"Description" +
+			strings.Repeat(" ", 25-len("Description")) +
+			" | " + "Change" + "\n"
+	default:
+		return "", errors.New("")
+	}
+	return out, nil
+}
+
+func doStuffWithEntries(entriesCopy []Entry) {
+	map1 := map[bool]int{true: 0, false: 1}
+	map2 := map[bool]int{true: -1, false: 1}
+	entriesCopy2 := entriesCopy
+	for len(entriesCopy2) > 1 {
+		first, rest := entriesCopy2[0], entriesCopy2[1:]
+		success := false
+		for !success {
+			success = true
+			for i, e := range rest {
+				if (map1[e.Date == first.Date]*map2[e.Date < first.Date]*4 +
+					map1[e.Description == first.Description]*map2[e.Description < first.Description]*2 +
+					map1[e.Change == first.Change]*map2[e.Change < first.Change]*1) < 0 {
+					entriesCopy2[0], entriesCopy2[i+1] = entriesCopy2[i+1], entriesCopy2[0]
+					success = false
+				}
+			}
+		}
+		entriesCopy2 = entriesCopy2[1:]
+	}
+}
+
+func buildCentsStrAndParts(
+	cents int,
+	a string,
+	negative bool,
+	delim1 string,
+	delim2 string,
+	delim3 string,
+) string {
+	centsStr := getCentsStr(cents)
+	rest := centsStr[:len(centsStr)-2]
+	var parts []string
+	for len(rest) > 3 {
+		parts = append(parts, rest[len(rest)-3:])
+		rest = rest[:len(rest)-3]
+	}
+
+	if len(rest) > 0 {
+		parts = append(parts, rest)
+	}
+
+	for i := len(parts) - 1; i >= 0; i-- {
+		a += parts[i] + delim1
+	}
+
+	a = a[:len(a)-1]
+	a += delim2
+	a += centsStr[len(centsStr)-2:]
+	if negative {
+		a += delim3
+	} else {
+		a += " "
+	}
+	return a
+}
+
+func getDate(locale string, entry Entry) string {
+	var date string
+	if locale == "nl-NL" {
+		date = entry.Date[8:10] + "-" + entry.Date[5:7] + "-" + entry.Date[0:4]
+	} else if locale == "en-US" {
+		date = entry.Date[5:7] + "/" + entry.Date[8:10] + "/" + entry.Date[0:4]
+	}
+	return date
+}
+
+func handleCurrency(a string, currency string, channel chan ChanEntry) string {
+	switch currency {
+	case "EUR":
+		a += "€"
+	case "USD":
+		a += "$"
+	default:
+		channel <- ChanEntry{e: errors.New("")}
+	}
+	return a
+}
+
+func buildA(
+	locale, currency string,
+	channel chan ChanEntry,
+	cents int,
+	negative bool,
+) (string, int) {
+	var a string
+	switch locale {
+	case "nl-NL":
+		a = handleCurrency(a, currency, channel)
+		a += " "
+		a = buildCentsStrAndParts(cents, a, negative, ".", ",", "-")
+	case "en-US":
+		if negative {
+			a += "("
+		}
+		a = handleCurrency(a, currency, channel)
+		a = buildCentsStrAndParts(cents, a, negative, ",", ".", ")")
+	default:
+		channel <- ChanEntry{e: errors.New("")}
+	}
+
+	var al int
+	for range a {
+		al++
+	}
+	return a, al
 }
