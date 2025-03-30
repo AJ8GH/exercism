5e3fbaeeb3acb1a991659227cbc69bff032afe11
Adam Jonas <adamjonas25@gmail.com>
2025-03-30 21:43:17 +0100

    Solve grade-school

--- go/grade-school/grade_school.go
+++ go/grade-school/grade_school.go
@@ -1,19 +1,64 @@
 package school
 
-// Define the Grade and School types here.
+import (
+	"slices"
+)
+
+type (
+	School struct {
+		gradesMap map[int]*Grade
+	}
+
+	Grade struct {
+		level    int
+		students []string
+	}
+)
 
 func New() *School {
-	panic("Please implement the New function")
+	return &School{gradesMap: map[int]*Grade{}}
 }
 
 func (s *School) Add(student string, grade int) {
-	panic("Please implement the Add function")
+	g := s.gradesMap[grade]
+	if g == nil {
+		g = &Grade{level: grade, students: []string{student}}
+		s.gradesMap[grade] = g
+		return
+	}
+	for _, v := range g.students {
+		if v == student {
+			return
+		}
+	}
+	g.students = append(g.students, student)
+	slices.Sort(g.students)
 }
 
 func (s *School) Grade(level int) []string {
-	panic("Please implement the Grade function")
+	g := s.gradesMap[level]
+
+	if g == nil {
+		return []string{}
+	}
+	return g.students
 }
 
-func (s *School) Enrollment() []Grade {
-	panic("Please implement the Enrollment function")
+func (s *School) Enrollment() (grades []Grade) {
+	for _, v := range s.gradesMap {
+		grades = append(grades, *v)
+	}
+	slices.SortFunc(grades, sortGrade)
+	return grades
+}
+
+func sortGrade(a, b Grade) int {
+	switch {
+	case a.level < b.level:
+		return -1
+	case a.level > b.level:
+		return 1
+	default:
+		return 0
+	}
 }
